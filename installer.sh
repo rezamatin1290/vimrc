a9d870d0008097549db2e11ece7b7aa75f3b2056
Iman Seyed <43682414+mzd245@users.noreply.github.com>
2019-07-02 16:58:15 +0430

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash 
 
 USER_NAME=$(who am i | awk {'print $1'})
-
+START_PWD=$PWD
 if [ "$1" != "help" ];then
     if [ ! -d /home/$USER_NAME/.vim ];then
         mkdir /home/$USER_NAME/.vim 
@@ -19,12 +19,27 @@ function vimrc_installer(){
     echo "$(tput setaf 2)[✔] Vimrc installed successfully!"
 }
 function cvim_installer(){ 
-    wget -O cvim.zip https://www.vim.org/scripts/download_script.php?src_id=21803 
-    OLD_PWD=$PWD
-    cd /home/$USER_NAME/.vim
-    echo "$(tput setaf 2)[+] Extracting cvim.zip ..."
-    unzip $OLD_PWD/cvim.zip
-    echo "$(tput setaf 2)[✔] C-support installation done !"
+    if [ ! -d /home/$USER_NAME/.vim/c-support ];then
+        wget -O cvim.zip https://www.vim.org/scripts/download_script.php?src_id=21803 
+        cd /home/$USER_NAME/.vim
+        echo "$(tput setaf 2)[+] Extracting cvim.zip ..."
+        unzip $START_PWD/cvim.zip
+        if [ "$?" = 0 ];then 
+            echo "$(tput setaf 2)[✔] C-support installation done !"
+        else 
+            echo -e "$(tput setaf 1)[✗] Unzip package not found ...\nInstall unzip from your repo and try 'fix_cvim' Option." 
+        fi
+    else 
+        echo "$(tput setaf 1)[✗] C-support plugin exist !"
+    fi
+}
+function fix_cvim(){
+    unzip $START_PWD/cvim.zip
+        if [ "$?" = 0 ];then 
+            echo "$(tput setaf 2)[✔] C-support installation done !"
+        else 
+            echo -e "$(tput setaf 1)[✗] Unzip package not found ...\nInstall unzip from your repo and try 'fix_cvim' Option." 
+        fi
 }
 function help(){
     echo "Usage: ./installer [OPTION]
@@ -42,10 +57,12 @@ function main(){
 
         only-cvim)
             cvim_installer
-            grep ':filetype plugin on' /home/$USER_NAME/.vimrc | sed '1d'
+            grep ':filetype plugin on' /home/$USER_NAME/.vimrc | sed -i '1d'
             echo ":filetype plugin on" >>  /home/$USER_NAME/.vimrc
             ;;
-
+        fix_cvim)
+            fix_cvim
+            ;;
         all)
             vimrc_installer
             cvim_installer
