86dc3eefcef9a4c3b0d955c4da13e8d3112cb46a
Iman Seyed <43682414+mzd245@users.noreply.github.com>
2019-07-04 17:28:10 +0430

    Comments added and some bug fixed

--- installer.sh
+++ installer.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/env bash 
 
-USER_NAME=$(who am i | awk {'print $1'})
-START_PWD=$PWD
-if [ "$1" != "help" ];then
-    if [ ! -d /home/$USER_NAME/.vim ];then
-        mkdir /home/$USER_NAME/.vim 
-    fi
-fi
+# Variables 
 
+USER_NAME=$(who am i | awk {'print $1'})
+VIMRC_LOC=$(find /home/$USER_NAME -iname ".vimrc")
+START_PWD=$PWD
+
+# Vimrc installer function
 function vimrc_installer(){
     echo "$(tput setaf 2)[+] Installing 'vim-plug' ..."
     sleep 1
     curl -fLo /home/$USER_NAME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    mv vimrc_file /home/$USER_NAME/.vimrc
+    mv vimrc_file $VIMRC_LOC
     sed -i 's/iman/'"$USER_NAME"'/g' vimrc_file
     echo "$(tput setaf 2)[+] Opening tmp.file for Plugin Installer ..."
     sleep 1.5
     vim +'PlugInstall' tmp.file
     
     echo "$(tput setaf 2)[✔] Vimrc installed successfully!"
-}
+} 
+
+# C-support plugin installer function 
 function cvim_installer(){ 
     if [ ! -d /home/$USER_NAME/.vim/c-support ];then
         wget -O cvim.zip https://www.vim.org/scripts/download_script.php?src_id=21803 
@@ -36,6 +37,8 @@ function cvim_installer(){
         echo "$(tput setaf 1)[✗] C-support plugin exist !"
     fi
 }
+
+# for unzip package missing
 function fix_cvim(){
     unzip $START_PWD/cvim.zip
         if [ "$?" = 0 ];then 
@@ -44,6 +47,8 @@ function fix_cvim(){
             echo -e "$(tput setaf 1)[✗] Unzip package not found ...\nInstall unzip from your repo and try 'fix_cvim' Option." 
         fi
 }
+
+# Help function
 function help(){
     echo "Usage: ./installer [OPTION]
 
@@ -53,29 +58,37 @@ function help(){
       help        display this help and exit"
 }
 
+# Main
 
- case "$1" in 
-     only-vimrc)
-          vimrc_installer
-          ;;
-     only-cvim)
-          cvim_installer
-          sed -i "s/:filetype plugin on//g" /home/$USER_NAME/.vimrc
-          echo ":filetype plugin on" >>  /home/$USER_NAME/.vimrc
-          ;;
-     fix_cvim)
-          fix_cvim
-          ;;
-      all)
-          vimrc_installer
-          cvim_installer
-          ;;
-      help)
-          help
-          ;;
-      *)
-          echo "installer.sh: missing operand
+# Creating directory ~/.vim directory  
+
+if [ "$1" != "help" ];then
+    if [ ! -d /home/$USER_NAME/.vim ];then
+        mkdir /home/$USER_NAME/.vim 
+    fi
+fi
+
+case "$1" in 
+   only-vimrc)
+        vimrc_installer
+        ;;
+   only-cvim)
+        cvim_installer 
+        sed -i "s/:filetype plugin on//g" $VIMRC_LOC
+        echo ":filetype plugin on" >>  $VIMRIC_LOC
+        ;;
+   fix_cvim)
+        fix_cvim
+        ;;
+    all)
+        vimrc_installer
+        cvim_installer
+        ;;
+    help)
+        help
+        ;;
+    *)
+        echo "installer.sh: missing operand
 Try './installer.sh help' for more information."
             exit -1
 esac
- 
